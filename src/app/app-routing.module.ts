1962daa4a1b0b00332998efeb019776b852c0f58
Aniruddha <aniruddha1712@gmail.com>
2022-08-04 15:26:16 +0530

    [Aniruddha]Added: login and signup component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Component/login/login.component';
+import { SignupComponent } from './Component/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'register', component:SignupComponent},
+  { path:'', redirectTo:"/login",pathMatch:"full"},
+  { path:'login', component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
